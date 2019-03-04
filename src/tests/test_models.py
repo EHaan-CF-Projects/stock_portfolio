a04e47b1a3f5f6b4b9f4a356a830f44d62f43e62
EvyHaan <evy.haan@gmail.com>
2019-03-04 14:38:30 -0800

    deprication warning taken care of and conftest created

---
+++ src/tests/test_models.py
@@ -0,0 +1,9 @@
+# from src.models import Company
+
+
+# def test_create_company(session):
+#     company = Company(name='Google', symbol='GOOG')
+#     assert city.id > 0
+
+def test_foo():
+    assert True
