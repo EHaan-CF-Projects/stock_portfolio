35fe14ee09e2e4736649536cccc27fe43186070b
EvyHaan <evy.haan@gmail.com>
2019-03-07 16:00:54 -0800

    users modelbuilt

--- src/models.py
+++ src/models.py
@@ -34,3 +34,33 @@ class Portfolio(db.Model):
 
     def __repr__(self):
         return '<Portfolio {}>'.format(self.name)
+
+
+class User(db.Model):
+    """
+    """
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), index=True, nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    portfolios = db.relationship('Portfolio', backref='user', lazy=True)
+
+    date_created = db.Column(db.DateTime, default=dt.now())
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = sha256_crypt.hash(password)
+
+    @classmethod
+    def check_password_hash(cls, user, password):
+        """
+        """
+        if user is not None:
+            if sha256_crypt.verify(password, user.password):
+                return True
+
+        return False
